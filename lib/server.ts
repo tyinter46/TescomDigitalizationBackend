e1fdae0c833ed6c9c3b8671b6ef88859a2856dc1
unknown <tyinter46@gmail.com>
2025-11-17 23:03:14 +0100

    corrected elastic search_URL

--- lib/server.ts
+++ lib/server.ts
@@ -1,9 +1,10 @@
 import http from 'http';
 import logger from './config/logger';
 import app, { PORT } from './config/app';
-// import importUsers from './scripts/importUsersToElastic';
+ import importUsers from './scripts/importUsersToElastic';
 
 http.createServer(app).listen(PORT, () => {
-  //  importUsers();
+  importUsers();
+  console.log(`Connected to ${PORT}`)
   logger.info(`Express server listening on port ${PORT} `);
 });
