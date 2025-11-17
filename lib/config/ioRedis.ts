3a4a26d8e64ec582e20b86b274a7d5e43727b681
unknown <tyinter46@gmail.com>
2025-11-17 22:19:06 +0100

    changed some env variables

--- lib/config/ioRedis.ts
+++ lib/config/ioRedis.ts
@@ -1,8 +1,8 @@
 import IORedis from 'ioredis';
 
 export const redisClient = new IORedis({
-  host: process.env.NODE_ENV === "development" ? process.env.REDIS_HOST : 'redis',
-  port: 6379,
+  host: process.env.REDIS_HOST ,
+  port: Number(process.env.REDIS_PORT),
   password: process.env.REDIS_PASSWORD,
   maxRetriesPerRequest: null,
   db: 1,
